38cdad67f3b3fb5c65a8f7a62c6f9b39c225d0b2
이규형 <rbgud2380@naver.com>
2023-09-16 20:10:35 +0900

    230916 commit

--- blog/src/components/List.vue
+++ blog/src/components/List.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Card" v-for="(project) in Projects" :key="project.number">
-    <router-link to="/detail"><h5>{{ project.title }}</h5></router-link>
+    <router-link to="/detail/0"><h5>{{ project.title }}</h5></router-link>
     <span>{{ project.content }}</span>
     <p>{{ project.date }}</p>
   </div>
