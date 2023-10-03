967c4f899344bbe1cf39a97650f322e6f6d80767
이규형 <rbgud2380@naver.com>
2023-10-03 22:34:40 +0900

    231003 commit

---
+++ blog/src/components/Comment.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>댓글~~</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ blog/src/components/Detail.vue
@@ -0,0 +1,24 @@
+<template>
+  $route.params.id
+  <div>
+    <h4>상세 페이지</h4>
+    <h5>{{ Projects[$route.params.id].title }}</h5>
+    <p>{{ Projects[$route.params.id].content }}</p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Detail',
+  data() {
+  },
+  props : {
+    Projects : Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
